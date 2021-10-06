2e16c7b90b0b35fd95ca7b7f25cb0df56e8dc0f4
Micah <mbro36@gmail.com>
2021-10-06 10:25:28 -0600

    quick minor style adjustment

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -6,7 +6,6 @@ function Layout() {
   return (
     <div className="container-fluid">
       <div className="row h-100">
-        <div className="col-md-2 side-bar">{/* <Menu /> */}</div>
         <div className="col">
           <main>
             <Routes />
